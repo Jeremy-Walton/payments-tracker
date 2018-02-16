c2999422d09cbad8d00aca30c5688ff5530da19f
Jeremy Walton <jthelegoman@gmail.com>
2018-02-16 16:53:29 -0500

    Make current controller change the active nav link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def nav_link(link_text, link_path)
+    current_class = current_page?(link_path) || controller?(link_path.split('/').second) ? 'active' : ''
+    link_to link_text, link_path, class: "nav-item nav-link #{current_class} #{controller_name}"
+  end
 end
