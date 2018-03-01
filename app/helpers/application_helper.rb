0ab804b0d2413a25207807dcb71b3f656be3ef13
Jeremy Walton <jthelegoman@gmail.com>
2018-02-28 20:35:06 -0500

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     current_class = current_page?(link_path) || controller?(link_path.split('/').second) ? 'active' : ''
     link_to link_text, link_path, class: "nav-item nav-link #{current_class} #{controller_name}"
   end
+
+  def flash_class(level)
+    case level
+    when 'notice' then 'alert alert-info'
+    when 'success' then 'alert alert-success'
+    when 'error' then 'alert alert-danger'
+    when 'alert' then 'alert alert-danger'
+    end
+  end
 end
