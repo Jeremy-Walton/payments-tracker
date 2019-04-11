4951073be8bd2f0883b62085be924719690b9ca5
Jeremy Walton <jthelegoman@gmail.com>
2019-04-11 14:27:24 +0200

    Add defaults to payment Show whether account is paid up to date

--- app/models/payment_account.rb
+++ app/models/payment_account.rb
@@ -2,4 +2,8 @@ class PaymentAccount < ApplicationRecord
   has_many :payments, dependent: :destroy
 
   validates :account_type, :name, :amount, presence: true
+
+  def up_to_date?
+    return payments.any? && payments.sort_by(&:date).last.date == Date.current.beginning_of_month ? 'Up to Date' : 'Payment Due'
+  end
 end
